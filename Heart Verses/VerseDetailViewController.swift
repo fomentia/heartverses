5d8841145e3239964205537dd8f54994b26bb3fa
ijwilliams <isaac@thewilliams.ws>
2015-07-22 16:23:00 -0400

    Wrap detail text

--- Heart Verses/VerseDetailViewController.swift
+++ Heart Verses/VerseDetailViewController.swift
@@ -5,18 +5,16 @@ class VerseDetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
     var detailItem: AnyObject? {
         didSet {
-            // Update the view.
             self.configureView()
         }
     }
 
     func configureView() {
-        // Update the user interface for the detail item.
         if let detail: AnyObject = self.detailItem {
             if let label = self.detailDescriptionLabel {
+                label.numberOfLines = 0
                 label.text = detail.valueForKey("text")!.description
             }
         }
@@ -24,15 +22,8 @@ class VerseDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
